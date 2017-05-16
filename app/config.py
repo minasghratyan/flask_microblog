54157331d26ef4276298aa6a1a4a6d6b7bd93617
Minas Ghratyan <migr1994migr@gmail.com>
2017-05-16 23:46:36 +0400

    mailing about followers

--- app/config.py
+++ app/config.py
@@ -16,13 +16,15 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 WHOOSH_BASE = os.path.join(basedir, 'search.db')
 
 # mail server settings
-MAIL_SERVER = 'localhost'
-MAIL_PORT = 25
-MAIL_USERNAME = None
-MAIL_PASSWORD = None
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = False
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 # administrator list
-ADMINS = ['you@example.com']
+ADMINS = ['migr1994migr@gmail.com']
 
 # pagination
 POSTS_PER_PAGE = 3
