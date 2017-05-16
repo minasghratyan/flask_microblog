30008ca7e9b053af0972c31a24cc701c3939b94d
Minas Ghratyan <migr1994migr@gmail.com>
2017-05-17 01:02:58 +0400

    Dates and Times

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,18 @@ from flask_login import LoginManager
 from flask_openid import OpenID
 from .config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, \
     MAIL_PASSWORD
+from flask_mail import Mail
+from .momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+app.jinja_env.globals['momentjs'] = momentjs
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+mail = Mail(app)
 
 if not app.debug:
     import logging
